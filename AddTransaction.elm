6251c661ff70b6fb55709ffdabdc13c89a9098e0
Théo Zimmermann <theo.zimmermann@ens.fr>
2016-06-05 12:10:15 +0200

    Add transaction in a dialog

---
+++ AddTransaction.elm
@@ -0,0 +1,169 @@
+module AddTransaction exposing (Model, init, Msg, update, view)
+
+
+import Date exposing (Date)
+import Date.Format as Date
+import DatePicker exposing (DatePicker, defaultSettings)
+import Html exposing (..)
+import Html.App as App
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Maybe
+import Maybe.Extra as Maybe
+import Positive exposing (Positive)
+import String
+
+
+main : Program Never
+main =
+  App.program
+    { init = init
+    , update =
+        (\msg model ->
+           let (model, cmd, _) = update msg model in
+           (model, cmd)
+        )
+    , view = view
+    , subscriptions = (\_ -> Sub.none)
+    }
+
+
+type alias Model =
+  { object : String
+  , value : Maybe (Positive Float)
+  , valueString : String
+  , datePicker : DatePicker
+  , date : Maybe Date
+  }
+
+
+init : (Model, Cmd Msg)
+init =
+  let
+    (dateModel, dateCmd) =
+      DatePicker.init
+        { defaultSettings |
+          placeholder = "Date"
+        , dateFormatter = Date.format "%e %b %Y"
+        , firstDayOfWeek = Date.Mon
+        }
+
+  in
+    { object = ""
+    , value = Nothing
+    , valueString = ""
+    , datePicker = dateModel
+    , date = Nothing
+    }
+  ! [ Cmd.map UpdateDate dateCmd
+    ]
+
+
+type Msg
+  = UpdateObject String
+  | UpdateValue String
+  | UpdateDate DatePicker.Msg
+  | Submit
+
+
+update : Msg -> Model -> (Model, Cmd Msg, Maybe (String, Positive Float, Date))
+update msg model =
+  case msg of
+    UpdateObject string ->
+      ( { model |
+          object = String.left 50 string
+        }
+      , Cmd.none
+      , Nothing
+      )
+
+    UpdateValue string ->
+      ( { model |
+          value =
+            (String.toFloat string |> Result.toMaybe)
+            `Maybe.andThen` Positive.fromNum
+        , valueString = string
+        }
+      , Cmd.none
+      , Nothing
+      )
+            
+    UpdateDate msg ->
+      let
+        (dateModel, dateCmd, date) =
+          DatePicker.update msg model.datePicker
+      in
+      ( { model |
+          datePicker = dateModel
+        , date = Maybe.oneOf [date, model.date]
+        }
+      , Cmd.map UpdateDate dateCmd
+      , Nothing
+      )
+
+    Submit ->
+      case (model.value , model.date) of
+        (Just value, Just date) ->
+          if String.isEmpty model.object then
+            (model, Cmd.none, Nothing)
+
+          else
+            ( model
+            , Cmd.none
+            , Just (model.object, value, date)
+            )
+
+        _ ->
+          (model, Cmd.none, Nothing)
+
+
+view : Model -> Html Msg
+view model =
+  let
+    notready =
+      String.isEmpty model.object
+      || Maybe.isNothing model.value
+      || Maybe.isNothing model.date
+  in
+    
+  Html.form [ onSubmit Submit ]
+    [ input
+        [ placeholder "Object"
+        , value model.object
+        , onInput UpdateObject
+        , inputStyle
+        ] []
+    , input
+        [ placeholder "Value"
+        , value model.valueString
+        , type' "number"
+        , onInput UpdateValue
+        , inputStyle
+        ] []
+    , App.map UpdateDate <| DatePicker.view model.datePicker
+    , input
+        [ type' "submit"
+        , disabled notready        
+        , style
+            [ ("background-color", if notready then "gray" else "#4CAF50")
+            , ("border", "none")
+            , ("color", "white")
+            , ("padding", "15px 32px")
+            , ("text-align", "center")
+            , ("font-weight", "bold")
+            , ("display", "inline-block")
+            , ("cursor", if notready then "not-allowed" else "pointer")
+            ]
+        ] []
+    ]
+
+
+inputStyle : Attribute msg
+inputStyle =
+  style
+    [ ("width", "90%")
+    , ("height", "30px")
+    , ("display", "block")
+    ]
+
+
--- LoggedIn.elm
+++ LoggedIn.elm
@@ -1,39 +1,37 @@
 module LoggedIn exposing (Model, init, view, Msg, update)
 
 
-import String
+import AddTransaction
 import Date exposing (Date)
 import Date.Format as Date
-import Time exposing (Time)
+import Dialog
 import Html exposing (..)
+import Html.App as App
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Json.Encode as Encode
-import Json.Decode as Decode exposing ((:=))
-import Task
 import Http
+import Json.Decode as Decode exposing ((:=))
+import Json.Encode as Encode
 import Kinvey exposing (Session)
 import MyKinvey exposing (..)
+import Positive exposing (Positive)
+import Task
 
 
 type alias Model =
   { session : Session
   , transactions : List Transaction
-  , newTransaction : Transaction
-  , dateValue : String
+  , newTransaction : Maybe Transaction
+  , addTransaction : Maybe AddTransaction.Model
   , recentError : String
   }
 
 
 type alias Transaction =
   { object : String
-  , value : Float
+  , value : Positive Float
   , date : Date
   }
-
-
-initTransaction : Transaction
-initTransaction = Transaction "" 0 (Date.fromTime 0)
 
 
 transactionTable : String
@@ -42,91 +40,46 @@ transactionTable = "transactions"
 
 init : Session -> (Model, Cmd Msg)
 init session =
-  Model session [] initTransaction "" "" !
+  Model session [] Nothing Nothing "" !
     [ Task.perform Error FetchTransactions
         <| getData session transactionTable
         <| Decode.object3 Transaction
             ("object" := Decode.string)
-            ("value" := Decode.float)
+            ("value" :=
+               Decode.customDecoder Decode.float
+                 (Positive.fromNum >> Result.fromMaybe "")
+            )
             ("date" := Decode.customDecoder Decode.string Date.fromString)
-    , Task.perform Error CurrentTime Time.now
     ]
 
 
 view : Model -> Html Msg
 view model =
-  let
-    addTransaction =
-      [ table
-          [ style
-              [ ("width", "100%")
-              ]
-          ]
-          [ (List.map (\attrs -> input (inputStyle :: attrs) []) >> tr')
-              [ [ placeholder "Object"
-                , onInput UpdateObject
-                , value model.newTransaction.object
-                ]
-              , [ placeholder "Value"
-                , type' "number"
-                , onInput UpdateValue
-                , value
-                    ( if model.newTransaction.value == 0 then
-                        ""
-                        
-                      else
-                        toString model.newTransaction.value
-                    )
-                ]
-              , [ placeholder "Date"
-                , type' "date"
-                , onInput UpdateDate
-                , value model.dateValue
-                ]
-              ]
-          ]
-      , div [ style [ ("text-align", "center") , ("margin", "10px 0") ] ]
-          [ button
-            [ onClick CreateTransaction
-            , style
-                [ ("background-color", "#4CAF50")
-                , ("border", "none")
-                , ("color", "white")
-                , ("padding", "15px 32px")
-                , ("text-align", "center")
-                , ("font-weight", "bold")
-                , ("display", "inline-block")
-                , ("cursor", "pointer")
-                ]
+  div []
+    [ button
+        [ onClick OpenAddTransaction ]
+        [ text "Add a new transaction" ]
+    , div
+        [ style [ ("color", "red") ] ]
+        [ text model.recentError ]
+    , h2 [] [ text "List of recent transactions" ]
+    , table
+        [ style
+            [ ("width", "100%")
+            , ("border", "1px solid black")
             ]
-            [ text "Add new transaction" ]
-          ]
-      , div [ style [ ("color", "red") ] ]
-          [ text model.recentError
-          ]
-      ]
-
-    listTransactions =
-      [ h2 [] [ text "List of recent transactions" ]
-      , table
-          [ style
-              [ ("width", "100%")
-              , ("border", "1px solid black")
-              ]
-          ]
-          <| List.map viewTransaction model.transactions
-      ]
-
-  in
-
-  div [] ( addTransaction ++ listTransactions )
+        ]
+        <| List.map viewTransaction model.transactions
+    , Dialog.view
+      <| Maybe.map addTransactionIntoConfig model.addTransaction
+    ]
 
 
 viewTransaction : Transaction -> Html msg
 viewTransaction { object , value , date } =
   tr'
     [ text object
-    , text (toString value)
+    , text (toString <| Positive.toNum value)
     , text (Date.format "%e %b %Y" date)
     ]
 
@@ -139,21 +92,18 @@ tr' lines =
   List.map (\e -> td [ style [ ("width", width) , ("padding", "3px 15px 0 5px") ] ] [e]) lines |> tr []
 
 
-inputStyle : Attribute a
-inputStyle =
-  style
-    [ ("width", "100%")
-    , ("height", "30px")
-    ]
+addTransactionIntoConfig : AddTransaction.Model -> Dialog.Config Msg
+addTransactionIntoConfig model =
+  { closeMessage = Nothing
+  , header = Just (text "Add a new transaction")
+  , body = Just (App.map AddTransactionMsg <| AddTransaction.view model)
+  , footer = Nothing
+  }
 
-
+    
 type Msg
-  = NoOp
-  | UpdateObject String
-  | UpdateValue String
-  | UpdateDate String
-  | CurrentTime Time
-  | CreateTransaction
+  = AddTransactionMsg AddTransaction.Msg
+  | OpenAddTransaction
   | CreatedTransaction ()
   | FetchTransactions (List Transaction)
   | Error Kinvey.Error
@@ -161,90 +111,66 @@ type Msg
 
 -- if update returns Nothing, it means the connection has failed
 update : Msg -> Model -> Maybe (Model, Cmd Msg)
-update msg ({ newTransaction } as model) =
+update msg model =
   case msg of
-    UpdateObject s ->
-      { model |
-        newTransaction = { newTransaction | object = s }
-      , recentError = ""
-      } |> updateStandard
-
-    UpdateValue s ->
-      case if String.isEmpty s then Ok 0 else String.toFloat s of
-        Ok value ->
-          if value > 0 || String.isEmpty s then
-            { model |
-              newTransaction = { newTransaction | value = value }
-            , recentError = ""
-            } |> updateStandard
-
-          else
-            { model |
-              recentError = "Value should be a positive number"
-            } |> updateStandard
-
-        Err _ ->
-          { model |
-            recentError = "Value should be a positive number"
-          } |> updateStandard
-
-    UpdateDate s ->
-      case
-        if String.isEmpty s then
-          Ok (Date.fromTime 0)
-
-        else
-          Date.fromString s
-      of
-        Ok date ->
-          { model |
-            newTransaction = { newTransaction | date = date }
-          , dateValue = s
-          , recentError = ""
-          } |> updateStandard
-
-        Err e ->
-          Debug.log "wierd 2"
-          { model |
-            dateValue = s
-          , recentError = e
-          } |> updateStandard
-
-    CurrentTime t ->
-        if model.dateValue == "" then
-          let today = Date.fromTime t in
-          { model |
-            newTransaction = { newTransaction | date = today }
-          , dateValue = Date.format "%Y-%m-%d" today
-          } |> updateStandard
-
-        else
-          model |> updateStandard
-
-    CreateTransaction ->
+    AddTransactionMsg msg ->
       let
-        transaction =
-          Encode.object
-            [ ("object", Encode.string model.newTransaction.object)
-            , ("value", Encode.float model.newTransaction.value)
-            , ( "date"
-              , Encode.string
-                  <| Date.formatISO8601 model.newTransaction.date
-              )
-            ]
-
+        updated =
+          Maybe.map (AddTransaction.update msg) model.addTransaction
       in
+        case updated of
+          Just (addTransaction, cmd, Nothing) ->
+            Just
+              ( { model | addTransaction = Just addTransaction }
+              , Cmd.map AddTransactionMsg cmd
+              )
 
-        Just
-          ( { model | recentError = "" }
-          , Task.perform Error CreatedTransaction
-              <| createData model.session transactionTable transaction
-          )
+          Just (addTransaction, cmd, Just (object, value, date)) ->
+            let
+              transaction =
+                Encode.object
+                  [ ("object", Encode.string object)
+                  , ("value", Encode.float <| Positive.toNum value)
+                  , ("date", Encode.string <| Date.formatISO8601 date)
+                  ]
+            in
+              Just
+                ( { model |
+                    addTransaction = Just addTransaction
+                  , newTransaction =
+                      Just { object = object, value = value, date = date }
+                  , recentError = ""
+                  }
+                ! [ Task.perform Error CreatedTransaction
+                    <| createData model.session transactionTable transaction
+                  , Cmd.map AddTransactionMsg cmd
+                  ]
+                )
+
+          Nothing ->
+            model |> updateStandard
+
+    OpenAddTransaction ->
+      let (addTransaction, cmd) = AddTransaction.init in
+      Just
+        ( { model |
+            addTransaction = Just addTransaction
+          }
+        , Cmd.map AddTransactionMsg cmd
+        )
 
     CreatedTransaction () ->
       { model |
-        transactions = model.newTransaction :: model.transactions
-      , newTransaction = initTransaction
+        transactions =
+          case model.newTransaction of
+            Just newT ->
+              newT :: model.transactions
+
+            Nothing ->
+              model.transactions
+          
+      , addTransaction = Nothing
+      , newTransaction = Nothing
       } |> updateStandard
 
     FetchTransactions t ->
@@ -259,9 +185,6 @@ update msg ({ newTransaction } as model) =
           { model
             | recentError = Kinvey.errorToString e
           } |> updateStandard
-
-    NoOp ->
-      model |> updateStandard
 
 
 updateStandard : a -> Maybe ( a, Cmd b )
